19e2ed384db89e3fb25392cf3b782d4758163262
Amy Johnson and Clem Freeman <clemfree@gmail.com>
2015-02-26 16:15:51 -0800

    Add README

---
+++ README.md
@@ -0,0 +1,19 @@
+Hacky News
+==========
+
+An [Angular.js](http://angularjs.org/) web app implementation of Hacker News. Written with the help of [Bootstrap](http://http://getbootstrap.com/).
+
+Usage
+-----
+
+In your browser, open the `index.html` file.
+
+About
+-----
+
+Written by Clem Freeman and Amy Johnson at [Epicodus](http://www.epicodus.com/).
+
+License
+-------
+
+This projected is licensed under the terms of the MIT license.
--- services/LinksFactory.js
+++ services/LinksFactory.js
@@ -1,9 +1,9 @@
 hackyNews.factory('LinksFactory', function LinksFactory() {
   var factory = {};
   factory.links = [
-{ title: 'Test1', url: 'http://www.google.com', userName: 'amy123', id: 1, points: 7, comments: [], rank: 0, date: new Date()},
-    { title: 'Test2', url: 'http://www.bing.com', userName: 'clem321', id: 2, points: 3, comments: [], rank: 0, date: new Date(2015,01,26,10)},
-  { title: 'Test3', url: 'http://www.bing.com', userName: 'clem321', id: 3, points: 10, comments: [], rank: 0, date: new Date(2015,01,20)}
+{ title: 'Test1', url: 'http://www.google.com', user: 'amy123', id: 1, points: 7, comments: [], rank: 0, date: new Date()},
+    { title: 'Test2', url: 'http://www.bing.com', user: 'clem321', id: 2, points: 3, comments: [], rank: 0, date: new Date(2015,01,26,10)},
+  { title: 'Test3', url: 'http://www.bing.com', user: 'clem321', id: 3, points: 10, comments: [], rank: 0, date: new Date(2015,01,20)}
   ];
 
   factory.addLink = function() {
